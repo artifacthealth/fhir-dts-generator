651ff15527784a7cca6902179a5df10a3dee40bd
Meir Gottlieb <meirgottlieb@gmail.com>
2015-07-01 07:17:17 -0600

    Moved declaration tests generation into 'main'

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 /// <reference path="./types.d.ts" />
 /// <reference path="../typings/mkdirp.d.ts" />
+/// <reference path="../typings/glob.d.ts" />
+
+import fs = require("fs");
+import path = require("path");
+import mkdirp = require("mkdirp");
+import glob = require("glob");
 
 import reader = require("./reader");
 import processor = require("./processor");
 import emitter = require("./emitters/declartionEmitter");
-import fs = require("fs");
-import mkdirp = require("mkdirp");
+
 
 var errors: string[];
 
@@ -28,6 +33,7 @@ if(errors.length == 0) {
 }
 
 reportErrors(errors);
+createDeclarationTests(specDir, outDir);
 
 process.exit(errors.length);
 
@@ -36,4 +42,17 @@ function reportErrors(errors: string[]): void {
     for(var i = 0; i < errors.length; i++) {
         console.log(errors[i]);
     }
+}
+
+function createDeclarationTests(specDir: string, outDir: string): void {
+    var examples = "",
+        count = 1;
+
+    glob.sync(path.join(specDir, "**/*example*.json")).forEach(filename => {
+        var example = JSON.parse(fs.readFileSync(filename, 'utf8'));
+        examples += "var example" + (count++) + ": fhir." + example.resourceType + " = " + JSON.stringify(example, null, "    ") + ";\n\n";
+    });
+
+    fs.writeFileSync(path.join(outDir, "fhir-tests.ts"), examples, "utf8");
+
 }
